49a92ee0b53bc6c89aa28614b96617d84605caa4
Jérémy Magrin <jeremy.magrin@gmail.com>
2021-11-03 15:21:03 -0400

    fix: accentuation problem with articles

--- src/utils/article.ts
+++ src/utils/article.ts
@@ -1,12 +1,15 @@
 import {Options} from '../types/general';
 
+import removeAccentuation from './removeAccentuation';
+
 import locales from '../locales';
 
 export default function getArticle(
   data: string = '',
   options: Options,
 ): [string | null, string] {
-  const input = data.trim();
+  const trimmedData = data.trim();
+  const input = removeAccentuation(trimmedData);
   const articles = locales[options.language.from].articles;
 
   for (const articleRegex of articles) {
@@ -14,7 +17,7 @@ export default function getArticle(
 
     if (match?.[1]) {
       const article = match?.[1]?.toLocaleLowerCase?.() ?? '';
-      const restOfIngredient = input.replace(article, '').trim();
+      const restOfIngredient = trimmedData.replace(article, '').trim();
 
       return [article?.toLowerCase?.()?.trim?.() || null, restOfIngredient];
     }
